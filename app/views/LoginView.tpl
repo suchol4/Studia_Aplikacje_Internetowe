2e215dbc24d41e5482aee48e8ab583c87081ff5c
jakub <jakub@Alienware>
2024-05-25 15:40:14 +0200

    Mała poprawa okna logowania.

--- app/views/LoginView.tpl
+++ app/views/LoginView.tpl
@@ -1,7 +1,7 @@
 {extends file="main.tpl"}
 
 {block name=content}
-<form action="{$conf->action_url}login" method="post"  class="pure-form pure-form-aligned bottom-margin">
+{*<form action="{$conf->action_url}login" method="post"  class="pure-form pure-form-aligned bottom-margin">
 	<legend>Logowanie do systemu</legend>
 	<fieldset>
         <div class="pure-control-group">
@@ -16,7 +16,27 @@
 			<input type="submit" value="zaloguj" class="pure-button pure-button-primary"/>
 		</div>
 	</fieldset>
-</form>	
+</form>	*}
+
+<section>
+        <form action="{$conf->action_url}login" method="post">
+            <div class="row gtr-50">
+                <div class="col-6 col-12-small">
+                    <label for="id_login">Użytkownik: </label>
+                    <input type="text" id="id_login" type="text" name="login"/>
+                </div>
+                <div class="col-6 col-12-small">
+                    <label for="id_pass">Hasło: </label>
+                    <input id="id_pass" type="password" name="pass" />
+                </div>
+                <div class="col-12">
+                    <ul class="actions">
+                        <li><input input type="submit" value="zaloguj" class="style1" /></li>
+                    </ul>
+                </div>
+            </div>
+        </form>
+    </section>	
 
 {include file='messages.tpl'}
 
--- templates_c/3bb18378c509f486dafe4c9dc467aaa8fb95aae4_0.file.LoginView.tpl.php
+++ templates_c/3bb18378c509f486dafe4c9dc467aaa8fb95aae4_0.file.LoginView.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.5.2, created on 2024-05-22 17:59:59
+/* Smarty version 4.5.2, created on 2024-05-25 15:39:00
   from 'C:\xampp\htdocs\kalkulator_bankowy\app\views\LoginView.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.5.2',
-  'unifunc' => 'content_664e167fb03332_58705653',
+  'unifunc' => 'content_6651e9f48000b5_35304460',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '3bb18378c509f486dafe4c9dc467aaa8fb95aae4' => 
     array (
       0 => 'C:\\xampp\\htdocs\\kalkulator_bankowy\\app\\views\\LoginView.tpl',
-      1 => 1716393598,
+      1 => 1716644338,
       2 => 'file',
     ),
   ),
@@ -21,47 +21,51 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:messages.tpl' => 1,
   ),
 ),false)) {
-function content_664e167fb03332_58705653 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6651e9f48000b5_35304460 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, true);
 ?>
 
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_945934998664e167fafe410_40651804', 'content');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_1861731076651e9f47fb459_50548792', 'content');
 ?>
 
 <?php $_smarty_tpl->inheritance->endChild($_smarty_tpl, "main.tpl");
 }
 /* {block 'content'} */
-class Block_945934998664e167fafe410_40651804 extends Smarty_Internal_Block
+class Block_1861731076651e9f47fb459_50548792 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'content' => 
   array (
-    0 => 'Block_945934998664e167fafe410_40651804',
+    0 => 'Block_1861731076651e9f47fb459_50548792',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 ?>
 
-<form action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
-login" method="post"  class="pure-form pure-form-aligned bottom-margin">
-	<legend>Logowanie do systemu</legend>
-	<fieldset>
-        <div class="pure-control-group">
-			<label for="id_login">Użytkownik: </label>
-			<input id="id_login" type="text" name="login"/>
-		</div>
-        <div class="pure-control-group">
-			<label for="id_pass">Hasło: </label>
-			<input id="id_pass" type="password" name="pass" /><br />
-		</div>
-		<div class="pure-controls">
-			<input type="submit" value="zaloguj" class="pure-button pure-button-primary"/>
-		</div>
-	</fieldset>
-</form>	
+
+<section>
+        <form action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
+login" method="post">
+            <div class="row gtr-50">
+                <div class="col-6 col-12-small">
+                    <label for="id_login">Użytkownik: </label>
+                    <input type="text" id="id_login" type="text" name="login" placeholder="Kwota kredytu"/>
+                </div>
+                <div class="col-6 col-12-small">
+                    <label for="id_pass">Hasło: </label>
+                    <input id="id_pass" type="password" name="pass" />
+                </div>
+                <div class="col-12">
+                    <ul class="actions">
+                        <li><input input type="submit" value="zaloguj" class="style1" /></li>
+                    </ul>
+                </div>
+            </div>
+        </form>
+    </section>	
 
 <?php $_smarty_tpl->_subTemplateRender('file:messages.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
